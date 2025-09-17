17892f983baf863f437f699c9d7859708f9409e2
ovitorlima1 <vitor.matheus@tpfe.com.br>
2025-09-17 09:15:51 -0300

    :wrench: chore: adicioando requerimentose servidor de API

---
+++ nj_server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+catalogo = {
+    "tomatoes": {
+        "units": "boxes",
+        "qty": 1000
+    },
+    "wine": {
+        "units": "bottles",
+        "qty": 500
+    }
+}
+
+app = FastAPI( title = "New Jersey API Server" )
+
+@app.get("/warehouse/{product}")
+async def load_truck(product, order_qty):
+    return { 
+        "product": product,
+        "order_qty": order_qty,
+        "units": catalogo[product]["units"]  
+    }
+
