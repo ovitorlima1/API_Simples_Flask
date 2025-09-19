c19267ef0da5e0d4a942b29e44f33ce1b8da4915
ovitorlima1 <vitor.matheus@tpfe.com.br>
2025-09-19 19:17:57 -0300

    :sparkles: feat: subindo servidor

--- nj_server.py
+++ nj_server.py
@@ -15,9 +15,12 @@ app = FastAPI( title = "New Jersey API Server" )
 
 @app.get("/warehouse/{product}")
 async def load_truck(product, order_qty):
+
+    catalogo[product]["qty"] -= int(order_qty)
     return { 
         "product": product,
         "order_qty": order_qty,
-        "units": catalogo[product]["units"]  
+        "units": catalogo[product]["units"],
+        "remaining_qty": catalogo[product]["qty"], 
     }
 
