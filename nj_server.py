3034325feee65ca5fb6f599b21850559e0217706
ovitorlima1 <vitor.matheus@tpfe.com.br>
2025-09-19 21:56:44 -0300

    :sparkles: feat: novo serviço adicioando ao nj_server

--- nj_server.py
+++ nj_server.py
@@ -1,26 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-catalogo = {
-    "tomatoes": {
-        "units": "boxes",
-        "qty": 1000
-    },
-    "wine": {
-        "units": "bottles",
-        "qty": 500
-    }
+# create data model
+class Product(BaseModel):
+	units: str
+	qty: int
+
+# create NJ warehouse catalog
+catalog = {
+	"tomatoes": Product(
+		units="boxes", 
+		qty=1000
+		),
+	"wine": Product(
+		units="bottles", 
+		qty=500
+		)
 }
 
-app = FastAPI( title = "New Jersey API Server" )
+# create API server
+app = FastAPI(title = "New Jersey API Server")
 
+# set up an endpoint for all catalog products
 @app.get("/warehouse/{product}")
 async def load_truck(product, order_qty):
+    """
+    deduct ordered product quantity from the catalog,
+    updating the inventory.
+    """
+    # the available product quantity
+    available = catalog[product].qty
 
-    catalogo[product]["qty"] -= int(order_qty)
-    return { 
+    # if order quantity is greater than the quantity at hand
+    if int(order_qty) > int(available):
+        # don't process the order - raise an exception
+        raise HTTPException(
+            status_code=400,
+            detail=f"Sorry, only {available} units are available, please try again…"
+        )
+    # otherwise - process order, and subtract order quantity from the inventory
+    catalog[product].qty -= int(order_qty)
+
+    # produce shipping confirmation
+    return {
         "product": product,
         "order_qty": order_qty,
-        "units": catalogo[product]["units"],
-        "remaining_qty": catalogo[product]["qty"], 
+        "units": catalog[product].units,
+        "remaining_qty": catalog[product].qty
     }
-
